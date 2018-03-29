1e54c00786acb1a9ecc50e31971dce896d5f007a
Bryan Castleman <castlemaninc@gmail.com>
2018-03-29 16:36:35 -0700

    adding jquery for scroll effect

--- README.md
+++ README.md
@@ -1,17 +1,17 @@
-# Layout Practice - {Layout 1}
+# Layout Practice - { Layout 1 - Responsive Portfolio }
 
 ## Description
 - For practice I re-imagined a responsive portfolio website in my own code. This is an exercise to make me a quicker web designer and Front-End Developer. 
 
 ### Major differences
 1. My version utilizes CSS Grid and Flexbox
-2. 
 
 ## Technologies Used
 
 - HTML5
 - CSS Grid, Flexbox, Media Queries for Responsive Layout
 - JavaScript
+- jQuery library
 - Meyerweb CSS Reset
 - Font Awesome
 
--- js/action.js
+++ js/action.js
@@ -1,5 +1,21 @@
-console.log("I'm working");
+$( document ).ready(function() {
 
-// as scroll begins site-header and mobile-header change color
+	$(window).scroll(function() {
+		// when user scrolls site-header and mobile-header change color
+	    if ($(this).scrollTop() > 0) {
+	        $(".site-header, .mobile-header").css('background-color', 'white');
+	        $(".site-header, .mobile-header").css('color', 'red');
+	    } else {
+	    	$(".site-header, .mobile-header").css('background-color', 'red');
+	    	$(".site-header, .mobile-header").css('color', 'white');
+	    }
 
-// Hero text in front of the background image fades out on scroll 
+	    // Hero text in front of the background image fades out on scroll 
+
+
+	});
+
+	// Add smooth scrolling animation when appropriate navigation links are clicked  
+
+    console.log( "ready!" );
+});
